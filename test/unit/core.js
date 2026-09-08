802f0479ddf1488593aad9709f43765d79c00f40
Seal <info@sealsecurity.io>
2026-09-08 09:23:52 +0000

    sonatype-2014-0026

--- test/unit/core.js
+++ test/unit/core.js
@@ -1341,6 +1341,36 @@ test("jQuery.parseHTML", function() {
 	equal( jQuery.parseHTML("<td><td>")[ 1 ].parentNode.nodeType, 11, "parentNode should be documentFragment" );
 });
 
+test( "jQuery.parseHTML(<a href>) - gh-2965", function() {
+	expect( 1 );
+
+	var html = "<a href='test.html'></a>",
+		href = jQuery.parseHTML( html )[ 0 ].href;
+
+	ok( /\/test\.html$/.test( href ), "href is not lost after parsing anchor" );
+});
+
+if ( jQuery.support.createHTMLDocument ) {
+	asyncTest( "jQuery.parseHTML", function() {
+		expect( 2 );
+
+		var img;
+
+		Globals.register("parseHTMLError");
+
+		jQuery.globalEval("parseHTMLError = false;");
+		img = jQuery.parseHTML( "<img src=x onerror='parseHTMLError = true'>" )[ 0 ];
+
+		notStrictEqual( img.ownerDocument, document,
+			"The markup is parsed in an inert document, not the live one" );
+
+		window.setTimeout(function() {
+			start();
+			equal( window.parseHTMLError, false, "onerror eventhandler has not been called." );
+		}, 2000);
+	});
+}
+
 test("jQuery.parseJSON", function() {
 	expect( 20 );
 
